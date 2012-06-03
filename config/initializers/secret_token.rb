00c3860fe10e5e34b780dec17a9c82617c4c1219
Joel Stimson <stimsojr@gmail.com>
2012-06-03 15:07:25 -0700

    first commit of blank repo with tests stripped so I can install rspec and cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joelsammich::Application.config.secret_token = '974adbf88f2a3012811a004bd74951e070f591f8ca6096586960edfb0600d88bc8159ca2490f60ffd2b2021f6a42ec094fd66e60ffcae3d2a14501c902cd5080'
