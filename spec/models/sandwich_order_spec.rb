bb726e00895bf9dcf46030f2104b13cdadf97c64
Joel Stimson <stimsojr@gmail.com>
2012-06-04 21:39:56 -0700

    rearraged orders spec

--- spec/models/sandwich_order_spec.rb
+++ spec/models/sandwich_order_spec.rb
@@ -1,28 +1,32 @@
 require 'spec_helper'
 
 describe SandwichOrder do
-  it "should not save without a sandwich type" do
-    subject.ordering_person = "Joel"
-    subject.sandwich_maker = "Chef"
-    subject.should_not be_valid
-  end
+  context "when not entering all require criteria" do
+    it "should not save without a sandwich type" do
+      subject.ordering_person = "Joel"
+      subject.sandwich_maker = "Chef"
+      subject.should_not be_valid
+    end
   
-  it "should not save without an ordering person" do
-    subject.sandwich_type = "Meat"
-    subject.sandwich_maker = "Chef"
-    subject.should_not be_valid
-  end
+    it "should not save without an ordering person" do
+      subject.sandwich_type = "Meat"
+      subject.sandwich_maker = "Chef"
+      subject.should_not be_valid
+    end
   
-  it "should not save without a sandwich maker" do
-    subject.ordering_person = "Joel"
-    subject.sandwich_type = "Veggie"
-    subject.should_not be_valid
+    it "should not save without a sandwich maker" do
+      subject.ordering_person = "Joel"
+      subject.sandwich_type = "Veggie"
+      subject.should_not be_valid
+    end
   end
-  
-  it "should save with all values filled in" do
-    subject.ordering_person = "Joel"
-    subject.sandwich_type = "Meat"
-    subject.sandwich_maker = "Chef"
-    subject.should be_valid
+
+  context "when all required fields are filled" do
+    it "should save with all values filled in" do
+      subject.ordering_person = "Joel"
+      subject.sandwich_type = "Meat"
+      subject.sandwich_maker = "Chef"
+      subject.should be_valid
+    end
   end
 end
