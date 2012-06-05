f27129e58c31b6d28e9caff5983ce514fb654d90
Joel Stimson <stimsojr@gmail.com>
2012-06-04 22:29:48 -0700

    moved time helper over to app helper. time, like the human spirit, should not be kept in such confines.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def alert_class key
+    div_class = case key.to_s
+    when "notice" || "success"
+      "alert-success"
+    when "error" || "warning"
+      "alert-error"
+    else
+      "alert-info"
+    end
+    div_class
+  end
+  
+  def this_year
+    Time.now.year
+  end
 end
--- app/helpers/sandwich_orders_helper.rb
+++ app/helpers/sandwich_orders_helper.rb
@@ -1,5 +1,2 @@
 module SandwichOrdersHelper
-  def this_year
-    Time.now.year
-  end
 end
