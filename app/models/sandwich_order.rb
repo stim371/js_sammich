4b015d6861932b13e5907f603b059ed04faacfc4
Joel Stimson <stimsojr@gmail.com>
2012-06-09 21:44:22 -0700

    added sand_url to accessor

--- app/models/sandwich_order.rb
+++ app/models/sandwich_order.rb
@@ -1,5 +1,5 @@
 class SandwichOrder < ActiveRecord::Base
-  attr_accessible :ordering_person, :sandwich_type, :sandwich_maker
+  attr_accessible :ordering_person, :sandwich_type, :sandwich_maker, :sandwich_url
   SANDWICH_TYPES = ["Meat", "Veggie", "Vegan", "Other"]
   
   validates :ordering_person, :presence => true
