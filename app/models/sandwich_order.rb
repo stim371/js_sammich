967eec1eaba0079ce7532b3898c2372cbd5b7775
Joel Stimson <stimsojr@gmail.com>
2012-06-10 02:53:06 -0700

    switched accessor to be new column name and ran migration to update schema as well.

--- app/models/sandwich_order.rb
+++ app/models/sandwich_order.rb
@@ -1,9 +1,13 @@
 class SandwichOrder < ActiveRecord::Base
-  attr_accessible :ordering_person, :sandwich_type, :sandwich_maker, :sandwich_url
+  attr_accessible :ordering_person, :sandwich_type, :sandwich_maker, :other_sandwich
   SANDWICH_TYPES = ["Meat", "Veggie", "Vegan", "Other"]
   
   validates :ordering_person, :presence => true
   validates :sandwich_maker, :presence => true
   validates :sandwich_type, :inclusion => { :in => SANDWICH_TYPES }
+  validates :other_sandwich, :presence => true, :if => :other_sandwich?
   
+  def other_sandwich?
+    sandwich_type == "Other"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120606023205) do
+ActiveRecord::Schema.define(:version => 20120610070201) do
 
   create_table "sandwich_orders", :force => true do |t|
     t.string   "ordering_person"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(:version => 20120606023205) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "sandwich_maker"
-    t.string   "sandwich_url"
+    t.string   "other_sandwich"
   end
 
 end
