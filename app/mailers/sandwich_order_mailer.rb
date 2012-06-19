3ae4f22b09b96fa8928360f2254bacadfc736559
Joel Stimson <stimsojr@gmail.com>
2012-06-19 00:31:29 -0700

    dolled up the orders index with the two new divs. might be getting away from bootstrap here...

--- app/mailers/sandwich_order_mailer.rb
+++ app/mailers/sandwich_order_mailer.rb
@@ -1,8 +1,9 @@
 class SandwichOrderMailer < ActionMailer::Base
   default from: "contact@uncoil.me"
   
-  def order_email(order)
+  def sandwich_order_email(order)
     @sandwich_order = order
+    @quote = Quote.random.html_safe
     mail(:to => order.sandwich_maker, :subject => "Sandwich Order from #{order.ordering_person}" )
   end
 end
