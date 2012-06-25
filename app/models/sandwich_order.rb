82bd0a918b38da2eac0042a45a01e4cff8ee48a6
Joel Stimson <stimsojr@gmail.com>
2012-06-24 22:27:57 -0700

    added validation for format of email

--- app/models/sandwich_order.rb
+++ app/models/sandwich_order.rb
@@ -1,9 +1,11 @@
 class SandwichOrder < ActiveRecord::Base
-  attr_accessible :ordering_person, :sandwich_type, :sandwich_maker, :other_sandwich
+  belongs_to :user
+  
+  attr_accessible :ordering_person, :sandwich_type, :sandwich_maker, :other_sandwich, :user
   SANDWICH_TYPES = ["Meat", "Veggie", "Vegan", "Other"]
   
   validates :ordering_person, :presence => true
-  validates :sandwich_maker, :presence => true
+  validates :sandwich_maker, :presence => true, :format => { :with =>  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
   validates :sandwich_type, :inclusion => { :in => SANDWICH_TYPES }
   validates :other_sandwich, :presence => true, :if => :other_sandwich?
   
