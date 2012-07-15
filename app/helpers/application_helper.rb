13d1527825dd79d88dac2825f5fb33179d981ef2
Joel Stimson <stimsojr@gmail.com>
2012-07-14 22:17:40 -0700

    added google analytics helper into layout file to track usage. Only displays in production env. closes #4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,20 @@ module ApplicationHelper
   def this_year
     Time.now.year
   end
+  
+  def google_analytics_js
+    content_tag(:script, :type => 'text/javascript') do
+        "var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-33387644-1']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();"
+    end if Rails.env.production?
+  end
+  
+  
 end
