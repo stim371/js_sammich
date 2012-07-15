2fa13f500ebf6c31642fc9cf2cbc947eaf2e8527
Joel Stimson <stimsojr@gmail.com>
2012-07-14 20:36:44 -0700

    updated routes so signed out users see the generic homepage, whereas logged in users get their dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Joelsammich::Application.routes.draw do
-  root :to => 'sandwich_orders#index'
+  get "pages/index", :as => "static_index"
+  get "pages/about_us", :as => "about_us"
+  get "pages/contact", :as => "contact"
 
+  authenticated :user do
+    root :to => 'sandwich_orders#index'
+  end
+  
+  root :to => 'pages#index'
+  
   devise_for :users
-
+  
   resources :sandwich_orders
   
   match 'success' => "sandwich_orders#success", :as => "success"
