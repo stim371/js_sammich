946f46f12dc3b5445e3b25f667e180db4627a7cc
Joel Stimson <stimsojr@gmail.com>
2012-07-14 21:46:30 -0700

    added static pages to the app with a controller and views. only using the homepage currently but could add a page for about or contact if needed. the homepage is referred to in the new routes file and is used when a person is not logged in.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def about_us
+  end
+
+  def contact
+  end
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about_us'" do
+    it "returns http success" do
+      get 'about_us'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "returns http success" do
+      get 'contact'
+      response.should be_success
+    end
+  end
+
+end
